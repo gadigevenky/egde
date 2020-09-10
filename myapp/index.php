1464ba93f8a2668bdbd0ec3a98aadfb32ff6b46e
Venky <gadigevenky@gmail.com>
2020-09-10 20:46:19 +0530

    vjain

--- myapp/index.php
+++ myapp/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <p>
-            Hello Cybage Team, Today is <?= date('l \t\h\e jS') ?>.
+            Hello Indian Team, Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
 </html>
